f84eebd0d7c3614dba5882e8e6e209ab05a33bd7
Kevin Johnson <kevin.johnson_12@hotmail.com>
2020-10-11 19:42:55 -0500

    Edited tic-tac-toe.js

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,64 +1,26 @@
 window.onload = function(){
 
-/*----- constants -----*/
-var squares = document.querySelectorAll("#board div");
-/*----- app"s state (variables) -----*/
+    /*----- constants -----*/
+    var squares = document.querySelectorAll("#board div");
+    /*----- app"s state (variables) -----*/
 
-var turn = 1;
-var state = ["","","","","","","","",""];
-var dict = {};
-var status = document.getElementById("status");
-
-
-/*----- cached element references -----*/
+    var turn = 1;
+    var state = ["","","","","","","","",""];
+    var dict = {};
+    var status = document.getElementById("status");
 
 
-for(var i = 0; i<squares.length; i++){
-    squares[i].classList.add("square");
-    dict[i] = squares[i];
-    squares[i].addEventListener("click", function(){
-        /*for(var key in dict){
-            if(this == squares[key]){
-                state[key] = turn;
-                console.log(dict);
-            }
-        }*/
-        console.log(dict);
-        console.log(state);
-            if( state[0] == turn && state[1] == turn && state[2] == turn ||
-                state[3] == turn && state[4] == turn && state[5] == turn ||
-                state[6] == turn && state[7] == turn && state[8] == turn ||
-                state[0] == turn && state[3] == turn && state[6] == turn ||
-                state[1] == turn && state[4] == turn && state[7] == turn ||
-                state[2] == turn && state[5] == turn && state[8] == turn ||
-                state[0] == turn && state[4] == turn && state[8] == turn ||
-                state[2] == turn && state[4] == turn && state[6] == turn){
-                this.classList.add("you-won");
-                    if(turn==1){
-                        status.textContent = "Congratulations! X is the Winner!";
-                    }else{
-                        status.textContent = "Congratulations! O is the Winner!";
-                    }
-            }
-        if(turn == 1 && this.innerHTML == ""){
-        this.innerHTML = "X";
-        this.classList.add("X")
-        state[i] = turn;
-        turn = 2;
-        }else if(turn == 2 && this.innerHTML == ""){
-        this.innerHTML = "O";
-        this.classList.add("O")
-        state[i] = turn;
-        turn = 1;
-        }
-    
-    });
-    squares[i].addEventListener("mouseover", function(){
-        this.classList.add("hover");
-    });
-    squares[i].addEventListener("mouseout",function(){
-        this.classList.remove("hover");
-    });
+    /*----- cached element references -----*/
+
+    var pos = 0;
+    squares.forEach(square => {
+        square.classList.add("square");
+        addListener(square, pos);
+        addHover(square);
+        removeHover(square);
+        pos = pos + 1;
+    })
+
     document.querySelector("button").addEventListener("click", function(){
         for(var square of squares){
             square.classList.remove("X");
@@ -70,14 +32,67 @@ for(var i = 0; i<squares.length; i++){
         state = ["","","","","","","","",""];
         status.textContent = "Move your mouse over a square and click to play an X or an O.";
     });
-}
+
+    function addListener(square, pos) {
+        square.addEventListener("click", function() {
+            if(turn == 1 && this.innerHTML == ""){
+                this.innerHTML = "X";
+                this.classList.add("X")
+                state[pos] = turn;
+                
+                if( state[0] == turn && state[1] == turn && state[2] == turn ||
+                    state[3] == turn && state[4] == turn && state[5] == turn ||
+                    state[6] == turn && state[7] == turn && state[8] == turn ||
+                    state[0] == turn && state[3] == turn && state[6] == turn ||
+                    state[1] == turn && state[4] == turn && state[7] == turn ||
+                    state[2] == turn && state[5] == turn && state[8] == turn ||
+                    state[0] == turn && state[4] == turn && state[8] == turn ||
+                    state[2] == turn && state[4] == turn && state[6] == turn){
+                    this.classList.add("you-won");
+                    if(turn==1){
+                        status.textContent = "Congratulations! X is the Winner!";
+                    }else{
+                        status.textContent = "Congratulations! O is the Winner!";
+                    }
+                }
+                this.style.pointerEvents = 'none';
+                turn = 2;
+            }else if(turn == 2 && this.innerHTML == ""){
+                this.innerHTML = "O";
+                this.classList.add("O")
+                state[pos] = turn;
+
+                if( state[0] == turn && state[1] == turn && state[2] == turn ||
+                    state[3] == turn && state[4] == turn && state[5] == turn ||
+                    state[6] == turn && state[7] == turn && state[8] == turn ||
+                    state[0] == turn && state[3] == turn && state[6] == turn ||
+                    state[1] == turn && state[4] == turn && state[7] == turn ||
+                    state[2] == turn && state[5] == turn && state[8] == turn ||
+                    state[0] == turn && state[4] == turn && state[8] == turn ||
+                    state[2] == turn && state[4] == turn && state[6] == turn){
+                    this.classList.add("you-won");
+                    if(turn==1){
+                        status.textContent = "Congratulations! X is the Winner!";
+                    }else{
+                        status.textContent = "Congratulations! O is the Winner!";
+                    }
+                }
+                this.style.pointerEvents = 'none';
+                turn = 1;
+            }
+        })
+    }
+
+    function addHover(square) {
+        square.addEventListener("mouseover", function(){
+            this.classList.add("hover");
+        });
+    }
+
+    function removeHover(square) {
+        square.addEventListener("mouseout",function(){
+            this.classList.remove("hover");
+        });
+    }
 
 }
-
-
-
-
-
-
-
-
